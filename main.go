cd99bcd05a6e18b14bb7cd02ad730e4dd93feab0
Kalle Jillheden <kalle.jillheden@iver.se>
2021-07-09 14:29:20 +0200

    Added config and backward compatability

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/iver-wharf/wharf-provider-github/docs"
+	"github.com/iver-wharf/wharf-provider-github/internal/httputils"
 
 	"github.com/gin-contrib/cors"
 	swaggerFiles "github.com/swaggo/files"
@@ -41,35 +43,44 @@ const buildDefinitionFileName = ".wharf-ci.yml"
 // @contact.email wharf@iver.se
 // @basePath /import
 func main() {
-	if err := loadEmbeddedVersionFile(); err != nil {
+	var (
+		config Config
+		err    error
+	)
+	if err = loadEmbeddedVersionFile(); err != nil {
 		fmt.Println("Failed to read embedded version.yaml file:", err)
+		os.Exit(1)
+	}
+	if config, err = loadConfig(); err != nil {
+		fmt.Println("Failed to read config:", err)
 		os.Exit(1)
 	}
 
 	docs.SwaggerInfo.Version = AppVersion.Version
 
+	if config.CA.CertsFile != "" {
+		client, err := httputils.NewClientWithCerts(config.CA.CertsFile)
+		if err != nil {
+			fmt.Println("Failed to get net/http.Client with certs:", err)
+			os.Exit(1)
+		}
+		http.DefaultClient = client
+	}
+
 	r := gin.Default()
 
-	allowCors, ok := os.LookupEnv("ALLOW_CORS")
-	if ok && allowCors == "YES" {
+	if config.HTTP.CORS.AllowAllOrigins {
 		fmt.Printf("Allowing CORS\n")
 		r.Use(cors.Default())
 	}
 
+	githubImporterModule{config: &config}.register(r)
+
 	r.GET("/", runPingHandler)
-	r.POST("/import/github", runGitHubHandler)
 	r.GET("/import/github/version", getVersionHandler)
 	r.GET("/import/github/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	_ = r.Run(getBindAddress())
-}
-
-func getBindAddress() string {
-	bindAddress, isBindAddressDefined := os.LookupEnv("BIND_ADDRESS")
-	if !isBindAddressDefined || bindAddress == "" {
-		return "0.0.0.0:8080"
-	}
-	return bindAddress
+	_ = r.Run(config.HTTP.BindAddress)
 }
 
 func runPingHandler(c *gin.Context) {
